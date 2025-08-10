236b15f47dc3163a9a36af4decf27b0c98e0ede8
Armağan Başkan <72693440+ABaskann@users.noreply.github.com>
2025-08-10 19:26:13 +0300

    Initial Commit

---
+++ PocketBook/PocketBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  PocketBookApp.swift
+//  PocketBook
+//
+//  Created by Armağan Başkan on 10.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct PocketBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PocketBookTests/PocketBookTests.swift
@@ -0,0 +1,17 @@
+//
+//  PocketBookTests.swift
+//  PocketBookTests
+//
+//  Created by Armağan Başkan on 10.08.2025.
+//
+
+import Testing
+@testable import PocketBook
+
+struct PocketBookTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
